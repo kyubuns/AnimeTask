3d6cb321ead79188a5975c53c4f53ae92cc1c272
kyubuns <kyubuns@gmail.com>
2018-09-30 20:15:18 +0900

    feat: 開発環境構築

---
+++ Assets/Development/Development.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Development : MonoBehaviour
+{
+    public void Start()
+    {
+        Debug.Log("Move Start");
+
+        Debug.Log("Move End");
+    }
+}
