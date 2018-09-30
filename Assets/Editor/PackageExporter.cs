cc91c33c24195f1dc26b3acd8d7438557e2fb616
kyubuns <kyubuns@gmail.com>
2018-09-30 21:44:47 +0900

    feat: PackageExporter

---
+++ Assets/Editor/PackageExporter.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Development
+{
+    public static class PackageExporter
+    {
+        [MenuItem("Dev/Export Package")]
+        public static void Export()
+        {
+            var directories = new[]
+            {
+                "Assets/AnimeTask",
+            };
+
+            // ReSharper disable once AssignNullToNotNullAttribute
+            var outputPath = Path.Combine(Path.GetDirectoryName(Application.dataPath), "AnimeTask.unitypackage");
+
+            AssetDatabase.ExportPackage(directories, outputPath, ExportPackageOptions.Recurse);
+
+            Debug.LogFormat("ExportPackage {0}", outputPath);
+        }
+    }
+}
