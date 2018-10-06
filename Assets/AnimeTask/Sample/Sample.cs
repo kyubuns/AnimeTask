3ddacf0335f1e5fea1575faf674c10995d222130
kyubuns <kyubuns@gmail.com>
2018-10-06 16:30:16 +0900

    feat: Sample01

---
+++ Assets/AnimeTask/Sample/Sample.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+namespace AnimeTask.Sample
+{
+    public class Sample : MonoBehaviour
+    {
+        public async Task Sample01()
+        {
+            Debug.Log("Play Sample01");
+
+            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+            await Anime.Play(
+                Easing.Create<OutCubic>(new Vector3(-5f, 0f, 0f), new Vector3(5f, 0f, 0f), 2f), TranslateTo.LocalPosition(cube.transform)
+            );
+
+            Destroy(cube);
+        }
+    }
+}
---
+++ Assets/AnimeTask/Sample/SampleMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AnimeTask.Sample
+{
+    public class SampleMenu : MonoBehaviour
+    {
+        [SerializeField] private Sample sample;
+        [SerializeField] private Dropdown dropdown;
+        private readonly List<Dropdown.OptionData> list = new List<Dropdown.OptionData>
+        {
+            new Dropdown.OptionData("Sample01"),
+        };
+
+        public void Start()
+        {
+            dropdown.options = list;
+            dropdown.onValueChanged.AddListener(x => { sample.Invoke(list[x].text, 0.0f); });
+            sample.Invoke(list[0].text, 0.0f);
+        }
+    }
+}
