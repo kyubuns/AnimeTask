cd883b3f878d2a0fd5134cbc6649ce8cdb5de8e3
kyubuns <kyubuns@gmail.com>
2018-10-06 16:43:33 +0900

    feat: TranslateToにGameObjectを渡せるようにした

--- Assets/AnimeTask/Sample/Sample.cs
+++ Assets/AnimeTask/Sample/Sample.cs
@@ -9,13 +9,13 @@ namespace AnimeTask.Sample
         {
             Debug.Log("Play Sample01");
 
-            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
             await Anime.Play(
-                Easing.Create<OutCubic>(new Vector3(-5f, 0f, 0f), new Vector3(5f, 0f, 0f), 2f), TranslateTo.LocalPosition(cube.transform)
+                Easing.Create<OutCubic>(new Vector3(-5f, 0f, 0f), new Vector3(5f, 0f, 0f), 2f), TranslateTo.LocalPosition(cube1)
             );
 
-            Destroy(cube);
+            Destroy(cube1);
         }
     }
 }
--- Assets/AnimeTask/Scripts/Translator/TransformTranslator.cs
+++ Assets/AnimeTask/Scripts/Translator/TransformTranslator.cs
@@ -5,17 +5,29 @@ namespace AnimeTask
     public static partial class TranslateTo
     {
         public static LocalPositionTranslator LocalPosition(Transform transform) => new LocalPositionTranslator(transform);
+        public static LocalPositionTranslator LocalPosition(GameObject gameObject) => new LocalPositionTranslator(gameObject.transform);
         public static LocalPositionXTranslator LocalPositionX(Transform transform) => new LocalPositionXTranslator(transform);
+        public static LocalPositionXTranslator LocalPositionX(GameObject gameObject) => new LocalPositionXTranslator(gameObject.transform);
         public static LocalPositionYTranslator LocalPositionY(Transform transform) => new LocalPositionYTranslator(transform);
+        public static LocalPositionYTranslator LocalPositionY(GameObject gameObject) => new LocalPositionYTranslator(gameObject.transform);
         public static LocalPositionZTranslator LocalPositionZ(Transform transform) => new LocalPositionZTranslator(transform);
+        public static LocalPositionZTranslator LocalPositionZ(GameObject gameObject) => new LocalPositionZTranslator(gameObject.transform);
         public static GlobalPositionTranslator GlobalPosition(Transform transform) => new GlobalPositionTranslator(transform);
+        public static GlobalPositionTranslator GlobalPosition(GameObject gameObject) => new GlobalPositionTranslator(gameObject.transform);
         public static GlobalPositionXTranslator GlobalPositionX(Transform transform) => new GlobalPositionXTranslator(transform);
+        public static GlobalPositionXTranslator GlobalPositionX(GameObject gameObject) => new GlobalPositionXTranslator(gameObject.transform);
         public static GlobalPositionYTranslator GlobalPositionY(Transform transform) => new GlobalPositionYTranslator(transform);
+        public static GlobalPositionYTranslator GlobalPositionY(GameObject gameObject) => new GlobalPositionYTranslator(gameObject.transform);
         public static GlobalPositionZTranslator GlobalPositionZ(Transform transform) => new GlobalPositionZTranslator(transform);
+        public static GlobalPositionZTranslator GlobalPositionZ(GameObject gameObject) => new GlobalPositionZTranslator(gameObject.transform);
         public static LocalScaleTranslator LocalScale(Transform transform) => new LocalScaleTranslator(transform);
+        public static LocalScaleTranslator LocalScale(GameObject gameObject) => new LocalScaleTranslator(gameObject.transform);
         public static LocalScaleXTranslator LocalScaleX(Transform transform) => new LocalScaleXTranslator(transform);
+        public static LocalScaleXTranslator LocalScaleX(GameObject gameObject) => new LocalScaleXTranslator(gameObject.transform);
         public static LocalScaleYTranslator LocalScaleY(Transform transform) => new LocalScaleYTranslator(transform);
+        public static LocalScaleYTranslator LocalScaleY(GameObject gameObject) => new LocalScaleYTranslator(gameObject.transform);
         public static LocalScaleZTranslator LocalScaleZ(Transform transform) => new LocalScaleZTranslator(transform);
+        public static LocalScaleZTranslator LocalScaleZ(GameObject gameObject) => new LocalScaleZTranslator(gameObject.transform);
     }
 
     public class LocalPositionTranslator : IValueTranslator<Vector3>
