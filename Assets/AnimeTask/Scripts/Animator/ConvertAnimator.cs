2786242d78142c2202bbb6ee1559b01ecf4e72ad
kyubuns <kyubuns@gmail.com>
2021-01-06 15:55:02 +0900

    feat: IAnimatorWithStartValueも型が変わらなければConvertして良い

--- Assets/AnimeTask/Scripts/Animator/ConvertAnimator.cs
+++ Assets/AnimeTask/Scripts/Animator/ConvertAnimator.cs
@@ -8,6 +8,11 @@ namespace AnimeTask
         {
             return new ConvertAnimator<T1, T2>(animator, func);
         }
+
+        public static ConvertAnimatorWithStartValue<T> Convert<T>(this IAnimatorWithStartValue<T> animator, Func<T, T> func)
+        {
+            return new ConvertAnimatorWithStartValue<T>(animator, func);
+        }
     }
 
     public class ConvertAnimator<T1, T2> : IAnimator<T2>
@@ -25,6 +30,23 @@ namespace AnimeTask
         {
             var (item1, item2) = animator.Update(time);
             return Tuple.Create(func(item1), item2);
+        }
+    }
+
+    public class ConvertAnimatorWithStartValue<T> : IAnimatorWithStartValue<T>
+    {
+        private readonly IAnimatorWithStartValue<T> animator;
+        private readonly Func<T, T> func;
+
+        public ConvertAnimatorWithStartValue(IAnimatorWithStartValue<T> animator, Func<T, T> func)
+        {
+            this.animator = animator;
+            this.func = func;
+        }
+
+        public IAnimator<T> Start(T startValue)
+        {
+            return new ConvertAnimator<T, T>(animator.Start(startValue), func);
         }
     }
 }
