cbab6de65471076b0e434d2c48f7584ddf20e1f2
kyubuns <kyubuns@gmail.com>
2021-01-06 16:37:14 +0900

    feat: この際だから昔の記述は消してしまう

--- Assets/AnimeTask/Scripts/Translator/ProgressTranslator.cs
+++ Assets/AnimeTask/Scripts/Translator/ProgressTranslator.cs
@@ -6,8 +6,7 @@ namespace AnimeTask
 {
     public static partial class TranslateTo
     {
-        public static ProgressTranslator<T> Progress<T>(IProgress<T> progress) => new ProgressTranslator<T>(progress);
-        public static UniTask ToProgress<T>(this IAnimator<T> animator, IProgress<T> progress, CancellationToken cancellationToken = default, IScheduler scheduler = default) => Anime.Play(animator, Progress(progress), scheduler, cancellationToken);
+        public static UniTask ToProgress<T>(this IAnimator<T> animator, IProgress<T> progress, CancellationToken cancellationToken = default, IScheduler scheduler = default) => Anime.Play(animator, new ProgressTranslator<T>(progress), scheduler, cancellationToken);
     }
 
     public class ProgressTranslator<T> : ITranslator<T>
