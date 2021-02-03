2167261919ce3b514cb13955ee6ee32d964943d5
kyubuns <kyubuns@gmail.com>
2021-02-03 17:25:34 +0900

    feat: BindTranslator

---
+++ Assets/AnimeTask/Scripts/Translator/BindTranslator.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+
+namespace AnimeTask
+{
+    public static partial class TranslateTo
+    {
+        public static UniTask ToBind<T>(this IAnimator<T> animator, Func<T> getter, Action<T> setter, CancellationToken cancellationToken = default, IScheduler scheduler = default) => Anime.Play(animator, new BindTranslator<T>(getter, setter), scheduler, cancellationToken);
+    }
+
+    public class BindTranslator<T> : IValueTranslator<T>
+    {
+        private readonly Func<T> getter;
+        private readonly Action<T> setter;
+
+        public T Current => getter();
+
+        public BindTranslator(Func<T> getter, Action<T> setter)
+        {
+            this.getter = getter;
+            this.setter = setter;
+        }
+
+        public void Update(T value)
+        {
+            setter(value);
+        }
+    }
+}
