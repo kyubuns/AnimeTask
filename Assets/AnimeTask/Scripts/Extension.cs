ab6af540e83adfd6f01552b36812282ca36ab1c1
kyubuns <kyubuns@gmail.com>
2021-03-14 10:41:23 +0900

    feat: AnimationCanceller

---
+++ Assets/AnimeTask/Scripts/Extension.cs
@@ -0,0 +1,50 @@
+using System.Threading;
+using UnityEngine;
+
+namespace AnimeTask
+{
+    public static class Extension
+    {
+        public static AnimationCanceller GetAnimationCanceller(this GameObject self)
+        {
+            var holder = self.GetComponent<AnimationCanceller>();
+            if (holder == null) holder = self.AddComponent<AnimationCanceller>();
+            return holder;
+        }
+
+        public static AnimationCanceller GetAnimationCanceller(this Component self)
+        {
+            return self.gameObject.GetAnimationCanceller();
+        }
+    }
+
+    public class AnimationCanceller : MonoBehaviour
+    {
+        private CancellationTokenSource cancellationTokenSource;
+
+        public void OnDestroy()
+        {
+            Cancel();
+        }
+
+        public void Cancel()
+        {
+            cancellationTokenSource?.Cancel();
+            cancellationTokenSource = null;
+        }
+
+        public CancellationTokenSource TokenSource
+        {
+            get
+            {
+                if (cancellationTokenSource == null)
+                {
+                    cancellationTokenSource = new CancellationTokenSource();
+                }
+                return cancellationTokenSource;
+            }
+        }
+
+        public CancellationToken Token => TokenSource.Token;
+    }
+}
