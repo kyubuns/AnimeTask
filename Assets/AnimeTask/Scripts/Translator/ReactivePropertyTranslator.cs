0bf7923c9f917e2f23f32391ba442671c42f6237
kyubuns <kyubuns@gmail.com>
2021-03-15 09:49:57 +0900

    feat: ReactivePropertyTranslator

---
+++ Assets/AnimeTask/Scripts/Translator/ReactivePropertyTranslator.cs
@@ -0,0 +1,30 @@
+#if ANIMETASK_UNIRX_SUPPORT
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UniRx;
+
+namespace AnimeTask
+{
+    public static partial class TranslateTo
+    {
+        public static UniTask ToReactiveProperty<T>(this IAnimator<T> animator, ReactiveProperty<T> reactiveProperty, CancellationToken cancellationToken = default, IScheduler scheduler = default) => Anime.Play(animator, new ReactivePropertyTranslator<T>(reactiveProperty), scheduler, cancellationToken);
+        public static UniTask ToReactiveProperty<T>(this IAnimatorWithStartValue<T> animator, ReactiveProperty<T> reactiveProperty, CancellationToken cancellationToken = default, IScheduler scheduler = default) => Anime.PlayTo(animator, new ReactivePropertyTranslator<T>(reactiveProperty), scheduler, cancellationToken);
+    }
+
+    public class ReactivePropertyTranslator<T> : IValueTranslator<T>
+    {
+        public T Current => reactiveProperty.Value;
+        private readonly ReactiveProperty<T> reactiveProperty;
+
+        public ReactivePropertyTranslator(ReactiveProperty<T> reactiveProperty)
+        {
+            this.reactiveProperty = reactiveProperty;
+        }
+
+        public void Update(T value)
+        {
+            reactiveProperty.Value = value;
+        }
+    }
+}
+#endif
