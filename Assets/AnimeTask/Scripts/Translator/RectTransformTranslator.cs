4e8f7c8b37034f6dc90d325acc32eb38b36e4353
kyubuns <kyubuns@gmail.com>
2021-09-02 16:52:34 +0900

    feat: ToSizeDelta

--- Assets/AnimeTask/Scripts/Translator/RectTransformTranslator.cs
+++ Assets/AnimeTask/Scripts/Translator/RectTransformTranslator.cs
@@ -134,6 +134,132 @@ namespace AnimeTask
             CheckAlive(graphic);
             return Anime.PlayTo(animator, new AnchoredPositionXTranslator(graphic.rectTransform, 1), scheduler, CancellationTokenSource.CreateLinkedTokenSource(graphic.GetCancellationTokenOnDestroy(), cancellationToken).Token, skipToken);
         }
+
+        [MustUseReturnValue]
+        public static UniTask ToSizeDelta(this IAnimator<Vector2> animator, GameObject gameObject, CancellationToken cancellationToken = default, SkipToken skipToken = default, IScheduler scheduler = default)
+        {
+            CheckAlive(gameObject);
+            return Anime.Play(animator, new SizeDeltaTranslator(gameObject.GetComponent<RectTransform>()), scheduler, CancellationTokenSource.CreateLinkedTokenSource(gameObject.GetCancellationTokenOnDestroy(), cancellationToken).Token, skipToken);
+        }
+
+        [MustUseReturnValue]
+        public static UniTask ToSizeDelta(this IAnimator<Vector2> animator, RectTransform rectTransform, CancellationToken cancellationToken = default, SkipToken skipToken = default, IScheduler scheduler = default)
+        {
+            CheckAlive(rectTransform);
+            return Anime.Play(animator, new SizeDeltaTranslator(rectTransform), scheduler, CancellationTokenSource.CreateLinkedTokenSource(rectTransform.GetCancellationTokenOnDestroy(), cancellationToken).Token, skipToken);
+        }
+
+        [MustUseReturnValue]
+        public static UniTask ToSizeDelta(this IAnimator<Vector2> animator, Graphic graphic, CancellationToken cancellationToken = default, SkipToken skipToken = default, IScheduler scheduler = default)
+        {
+            CheckAlive(graphic);
+            return Anime.Play(animator, new SizeDeltaTranslator(graphic.rectTransform), scheduler, CancellationTokenSource.CreateLinkedTokenSource(graphic.GetCancellationTokenOnDestroy(), cancellationToken).Token, skipToken);
+        }
+
+        [MustUseReturnValue]
+        public static UniTask ToSizeDelta(this IAnimatorWithStartValue<Vector2> animator, GameObject gameObject, CancellationToken cancellationToken = default, SkipToken skipToken = default, IScheduler scheduler = default)
+        {
+            CheckAlive(gameObject);
+            return Anime.PlayTo(animator, new SizeDeltaTranslator(gameObject.GetComponent<RectTransform>()), scheduler, CancellationTokenSource.CreateLinkedTokenSource(gameObject.GetCancellationTokenOnDestroy(), cancellationToken).Token, skipToken);
+        }
+
+        [MustUseReturnValue]
+        public static UniTask ToSizeDelta(this IAnimatorWithStartValue<Vector2> animator, RectTransform rectTransform, CancellationToken cancellationToken = default, SkipToken skipToken = default, IScheduler scheduler = default)
+        {
+            CheckAlive(rectTransform);
+            return Anime.PlayTo(animator, new SizeDeltaTranslator(rectTransform), scheduler, CancellationTokenSource.CreateLinkedTokenSource(rectTransform.GetCancellationTokenOnDestroy(), cancellationToken).Token, skipToken);
+        }
+
+        [MustUseReturnValue]
+        public static UniTask ToSizeDelta(this IAnimatorWithStartValue<Vector2> animator, Graphic graphic, CancellationToken cancellationToken = default, SkipToken skipToken = default, IScheduler scheduler = default)
+        {
+            CheckAlive(graphic);
+            return Anime.PlayTo(animator, new SizeDeltaTranslator(graphic.rectTransform), scheduler, CancellationTokenSource.CreateLinkedTokenSource(graphic.GetCancellationTokenOnDestroy(), cancellationToken).Token, skipToken);
+        }
+
+        [MustUseReturnValue]
+        public static UniTask ToSizeDeltaX(this IAnimator<float> animator, GameObject gameObject, CancellationToken cancellationToken = default, SkipToken skipToken = default, IScheduler scheduler = default)
+        {
+            CheckAlive(gameObject);
+            return Anime.Play(animator, new SizeDeltaXTranslator(gameObject.GetComponent<RectTransform>(), 0), scheduler, CancellationTokenSource.CreateLinkedTokenSource(gameObject.GetCancellationTokenOnDestroy(), cancellationToken).Token, skipToken);
+        }
+
+        [MustUseReturnValue]
+        public static UniTask ToSizeDeltaX(this IAnimator<float> animator, RectTransform rectTransform, CancellationToken cancellationToken = default, SkipToken skipToken = default, IScheduler scheduler = default)
+        {
+            CheckAlive(rectTransform);
+            return Anime.Play(animator, new SizeDeltaXTranslator(rectTransform, 0), scheduler, CancellationTokenSource.CreateLinkedTokenSource(rectTransform.GetCancellationTokenOnDestroy(), cancellationToken).Token, skipToken);
+        }
+
+        [MustUseReturnValue]
+        public static UniTask ToSizeDeltaX(this IAnimator<float> animator, Graphic graphic, CancellationToken cancellationToken = default, SkipToken skipToken = default, IScheduler scheduler = default)
+        {
+            CheckAlive(graphic);
+            return Anime.Play(animator, new SizeDeltaXTranslator(graphic.rectTransform, 0), scheduler, CancellationTokenSource.CreateLinkedTokenSource(graphic.GetCancellationTokenOnDestroy(), cancellationToken).Token, skipToken);
+        }
+
+        [MustUseReturnValue]
+        public static UniTask ToSizeDeltaX(this IAnimatorWithStartValue<float> animator, GameObject gameObject, CancellationToken cancellationToken = default, SkipToken skipToken = default, IScheduler scheduler = default)
+        {
+            CheckAlive(gameObject);
+            return Anime.PlayTo(animator, new SizeDeltaXTranslator(gameObject.GetComponent<RectTransform>(), 0), scheduler, CancellationTokenSource.CreateLinkedTokenSource(gameObject.GetCancellationTokenOnDestroy(), cancellationToken).Token, skipToken);
+        }
+
+        [MustUseReturnValue]
+        public static UniTask ToSizeDeltaX(this IAnimatorWithStartValue<float> animator, RectTransform rectTransform, CancellationToken cancellationToken = default, SkipToken skipToken = default, IScheduler scheduler = default)
+        {
+            CheckAlive(rectTransform);
+            return Anime.PlayTo(animator, new SizeDeltaXTranslator(rectTransform, 0), scheduler, CancellationTokenSource.CreateLinkedTokenSource(rectTransform.GetCancellationTokenOnDestroy(), cancellationToken).Token, skipToken);
+        }
+
+        [MustUseReturnValue]
+        public static UniTask ToSizeDeltaX(this IAnimatorWithStartValue<float> animator, Graphic graphic, CancellationToken cancellationToken = default, SkipToken skipToken = default, IScheduler scheduler = default)
+        {
+            CheckAlive(graphic);
+            return Anime.PlayTo(animator, new SizeDeltaXTranslator(graphic.rectTransform, 0), scheduler, CancellationTokenSource.CreateLinkedTokenSource(graphic.GetCancellationTokenOnDestroy(), cancellationToken).Token, skipToken);
+        }
+
+        [MustUseReturnValue]
+        public static UniTask ToSizeDeltaY(this IAnimator<float> animator, GameObject gameObject, CancellationToken cancellationToken = default, SkipToken skipToken = default, IScheduler scheduler = default)
+        {
+            CheckAlive(gameObject);
+            return Anime.Play(animator, new SizeDeltaXTranslator(gameObject.GetComponent<RectTransform>(), 1), scheduler, CancellationTokenSource.CreateLinkedTokenSource(gameObject.GetCancellationTokenOnDestroy(), cancellationToken).Token, skipToken);
+        }
+
+        [MustUseReturnValue]
+        public static UniTask ToSizeDeltaY(this IAnimator<float> animator, RectTransform rectTransform, CancellationToken cancellationToken = default, SkipToken skipToken = default, IScheduler scheduler = default)
+        {
+            CheckAlive(rectTransform);
+            return Anime.Play(animator, new SizeDeltaXTranslator(rectTransform, 1), scheduler, CancellationTokenSource.CreateLinkedTokenSource(rectTransform.GetCancellationTokenOnDestroy(), cancellationToken).Token, skipToken);
+        }
+
+        [MustUseReturnValue]
+        public static UniTask ToSizeDeltaY(this IAnimator<float> animator, Graphic graphic, CancellationToken cancellationToken = default, SkipToken skipToken = default, IScheduler scheduler = default)
+        {
+            CheckAlive(graphic);
+            return Anime.Play(animator, new SizeDeltaXTranslator(graphic.rectTransform, 1), scheduler, CancellationTokenSource.CreateLinkedTokenSource(graphic.GetCancellationTokenOnDestroy(), cancellationToken).Token, skipToken);
+        }
+
+        [MustUseReturnValue]
+        public static UniTask ToSizeDeltaY(this IAnimatorWithStartValue<float> animator, GameObject gameObject, CancellationToken cancellationToken = default, SkipToken skipToken = default, IScheduler scheduler = default)
+        {
+            CheckAlive(gameObject);
+            return Anime.PlayTo(animator, new SizeDeltaXTranslator(gameObject.GetComponent<RectTransform>(), 1), scheduler, CancellationTokenSource.CreateLinkedTokenSource(gameObject.GetCancellationTokenOnDestroy(), cancellationToken).Token, skipToken);
+        }
+
+        [MustUseReturnValue]
+        public static UniTask ToSizeDeltaY(this IAnimatorWithStartValue<float> animator, RectTransform rectTransform, CancellationToken cancellationToken = default, SkipToken skipToken = default, IScheduler scheduler = default)
+        {
+            CheckAlive(rectTransform);
+            return Anime.PlayTo(animator, new SizeDeltaXTranslator(rectTransform, 1), scheduler, CancellationTokenSource.CreateLinkedTokenSource(rectTransform.GetCancellationTokenOnDestroy(), cancellationToken).Token, skipToken);
+        }
+
+        [MustUseReturnValue]
+        public static UniTask ToSizeDeltaY(this IAnimatorWithStartValue<float> animator, Graphic graphic, CancellationToken cancellationToken = default, SkipToken skipToken = default, IScheduler scheduler = default)
+        {
+            CheckAlive(graphic);
+            return Anime.PlayTo(animator, new SizeDeltaXTranslator(graphic.rectTransform, 1), scheduler, CancellationTokenSource.CreateLinkedTokenSource(graphic.GetCancellationTokenOnDestroy(), cancellationToken).Token, skipToken);
+        }
     }
 
     public class AnchoredPositionTranslator : IValueTranslator<Vector2>
@@ -170,6 +296,43 @@ namespace AnimeTask
             var p = transform.anchoredPosition;
             p[Index] = value;
             transform.anchoredPosition = p;
+        }
+    }
+
+    public class SizeDeltaTranslator : IValueTranslator<Vector2>
+    {
+        private readonly RectTransform transform;
+        public Vector2 Current => transform.sizeDelta;
+
+        public SizeDeltaTranslator(RectTransform transform)
+        {
+            this.transform = transform;
+        }
+
+        public void Update(Vector2 value)
+        {
+            transform.sizeDelta = value;
+        }
+    }
+
+    public class SizeDeltaXTranslator : IValueTranslator<float>
+    {
+        public int Index { get; }
+
+        private readonly RectTransform transform;
+        public float Current => transform.sizeDelta[Index];
+
+        public SizeDeltaXTranslator(RectTransform transform, int index)
+        {
+            Index = index;
+            this.transform = transform;
+        }
+
+        public void Update(float value)
+        {
+            var p = transform.sizeDelta;
+            p[Index] = value;
+            transform.sizeDelta = p;
         }
     }
 }
